7b2ec352e851ce84764f0ca20c85634ba682760c
Namratha Kondi <namratha.9966@gmail.com>
2022-09-11 00:29:28 -0400

    Initial commit

---
+++ src/main/java/This/API/helps/in/rewards/calculation/RewardsManagementApiApplication.java
@@ -0,0 +1,13 @@
+package This.API.helps.in.rewards.calculation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RewardsManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RewardsManagementApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/This/API/helps/in/rewards/calculation/RewardsManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package This.API.helps.in.rewards.calculation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RewardsManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
